b400a888e79cdadc8a9d5bb44cbaddcb38399396
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-09-18 14:14:50 -0700

    get profile by handle

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileAbout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>to do: PROFILE ABOUT</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileAbout;
